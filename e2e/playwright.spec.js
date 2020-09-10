2481b103a247f80f3d493c6b4967d95e8a3ba631
Max Netterberg <max.netterberg@gmail.com>
2020-09-10 14:18:58 +0200

    playwright is now working

--- e2e/playwright.spec.js
+++ e2e/playwright.spec.js
@@ -1,32 +1,22 @@
-const { webkit, chromium, firefox } = require('playwright')
 const expect = require('expect')
 
-const browsers = { webkit, chromium, firefox }
+let page
 
-;(async () => {
-  for (let key of Object.keys(browsers)) {
-    let browser
-    let page
-    beforeAll(async () => {
-      browser = await browsers[key].launch({ headless: true, slowMo: 100 })
-    })
-    afterAll(async () => {
-      await browser.close()
-    })
-    beforeEach(async () => {
-      page = await browser.newPage()
-    })
-    afterEach(async () => {
-      await page.close()
-    })
-    it('should work', async () => {
-      await page.goto('http://localhost:8080/')
-      expect(await page.title()).toBe('app')
-    })
-    // await page.goto('http://localhost:8080/')
-    // await page.fill('#title-input', 'This is my first post')
-    // await page.fill('#body-input', 'This is the body of my post')
-    // await page.click('#submit-input')
-    // await browser.close()
-  }
-})()
+afterAll(async () => {
+  await browser.close()
+})
+beforeEach(async () => {
+  page = await browser.newPage()
+})
+afterEach(async () => {
+  await page.close()
+})
+it('should work', async () => {
+  await page.goto('http://localhost:8080/')
+  expect(await page.title()).toBe('app')
+})
+// await page.goto('http://localhost:8080/')
+// await page.fill('#title-input', 'This is my first post')
+// await page.fill('#body-input', 'This is the body of my post')
+// await page.click('#submit-input')
+// await browser.close()
