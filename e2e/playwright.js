f16e176dd201e5f744f2226f1c4fd89ecd6a7e7c
Max Netterberg <max.netterberg@gmail.com>
2020-09-10 13:54:08 +0200

    testing

--- e2e/playwright.js
+++ e2e/playwright.js
@@ -4,10 +4,12 @@ const browsers = { webkit, chromium, firefox }
 
 ;(async () => {
   for (let key of Object.keys(browsers)) {
-    const browser = await browsers[key].launch()
+    const browser = await browsers[key].launch({ headless: true, slowMo: 100 })
     const page = await browser.newPage()
-    await page.goto('https://google.com/')
-    await page.screenshot({ path: `example-${key}.png` })
+    await page.goto('http://localhost:8080/')
+    await page.fill('#title-input', 'This is my first post')
+    await page.fill('#body-input', 'This is the body of my post')
+    await page.click('#submit-input')
     await browser.close()
   }
 })()
