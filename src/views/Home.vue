55d9ab2e8146d8666e03f1e9d587f5872f0e499e
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2020-09-10 13:33:14 +0200

    Mockup for creating new posts created.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>Posts</h1>
+    <div class="new-post">
+      <h2>New post</h2>
+      <input type="text" placeholder="Post title" v-model="postTitle" />
+      <textarea type="text" placeholder="Post content" v-model="postBody"></textarea>
+      <button class="submit-post" @click="submitPost">Submit</button>
+    </div>
     <div class="posts">
       <Post
         v-for="post in posts"
@@ -15,14 +21,29 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 // @ is an alias to /src
-import Post from "@/components/Post.vue";
+import Post from '@/components/Post.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   data() {
     return {
       posts: [],
+    }
+  },
+  setup() {
+    // new post
+    const postTitle = ref('')
+    const postBody = ref('')
+    const submitPost = () => {
+      console.log('title', postTitle.value)
+      console.log('body', postBody.value)
+    }
+    return {
+      postTitle,
+      postBody,
+      submitPost,
     }
   },
   components: {
@@ -31,6 +52,39 @@ export default {
   async created() {
     const resp = await fetch('http://jsonplaceholder.typicode.com/posts')
     this.posts = (await resp.json()).slice(0, 5)
-  }
-};
+  },
+}
 </script>
+
+<style scoped lang="scss">
+.new-post {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  margin: 2em;
+}
+
+input,
+textarea {
+  font-size: 1.2em;
+  margin: 0.25em 0;
+  border: 2px solid #ddd;
+  border-radius: 5px;
+  padding: 0.25em;
+}
+textarea {
+  min-height: 5em;
+}
+button {
+  padding: 0.5em 0.25em;
+  font-size: 1.2em;
+  cursor: pointer;
+  border: none;
+  border-radius: 3px;
+  transition: background-color 0.2s ease;
+  background-color: #eee;
+  &:hover {
+    background-color: #ccc;
+  }
+}
+</style>
