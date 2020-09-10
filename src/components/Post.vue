5e259316f9d5308e19f797582068d12da949ae07
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2020-09-10 13:15:35 +0200

    Added posts.

---
+++ src/components/Post.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="post">
+    <div class="header">
+      <h2 class="title">{{ title }}</h2>
+      <p class="post-id">{{ id }}</p>
+    </div>
+    <p class="body">{{ body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['userId', 'id', 'title', 'body'],
+}
+</script>
+
+<style lang="scss" scoped>
+.post {
+  margin: 1em;
+  padding: 1em;
+  background-color: #f9f9f9;
+  -webkit-box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.5);
+  -moz-box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.5);
+  box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.5);
+  text-align: left;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Posts</h1>
+    <div class="posts">
+      <Post
+        v-for="post in posts"
+        :key="post.id"
+        :id="post.id"
+        :userId="post.userId"
+        :title="post.title"
+        :body="post.body"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Post from "@/components/Post.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      posts: [],
+    }
+  },
   components: {
-    HelloWorld
+    Post,
+  },
+  async created() {
+    const resp = await fetch('http://jsonplaceholder.typicode.com/posts')
+    this.posts = (await resp.json()).slice(0, 5)
   }
 };
 </script>
